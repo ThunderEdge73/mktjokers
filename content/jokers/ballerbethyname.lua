f70c3fbe1cb64cd7762141d8492dfed89c5c68fa
Jairus Azarael Bona <webmaster@milkyway.moe>
2025-09-27 00:18:10 +0800

    Adjusted the baller sprite
    hope this helps :3

--- content/jokers/ballerbethyname.lua
+++ content/jokers/ballerbethyname.lua
@@ -2,7 +2,6 @@ SMODS.Joker{ --Baller Be Thy Name
     key = "ballerbethyname",
     config = {
         extra = {
-            Xmult = math.pi
         }
     },
     loc_txt = {
@@ -23,9 +22,9 @@ SMODS.Joker{ --Baller Be Thy Name
     soul_pos = {x = 3, y = 6, draw=function (card, scale_mod, rotate_mod)
         if card.children.floating_sprite then
             card.children.floating_sprite.T.w = 1.4
-            rotate_mod = 0
+            rotate_mod = -G.TIMERS.REAL * 1.235
             local sc = 0
-            local xm = -0.25
+            local xm = -0.70
             local ym = 0
             card.children.floating_sprite:draw_shader('dissolve', nil, nil,nil,card.children.center,sc, rotate_mod,xm,ym,nil, 0.6)
         end
@@ -43,31 +42,47 @@ SMODS.Joker{ --Baller Be Thy Name
         ["milkys_jokers"] = true 
     },
 
-
+    set_badges = function(self, card, badges)
+ 		badges[#badges+1] = create_badge('Immutable', G.C.ORANGE, G.C.WHITE, 1 )
+ 	end,
 
     calculate = function(self, card, context)
             -- give xmult 
             if context.joker_main then
-                local jackCount = 0
-                local queenCount = 0
-                local kingCount = 0
+                local cardswithfaces = {}
+                -- local jackCount = 0
+                -- local queenCount = 0
+                -- local kingCount = 0
 
                 for _, card in ipairs(context.scoring_hand) do
-                    if card:get_id() == 11 then --counting jacks
-                        jackCount = jackCount + 1
+                    if card:is_face() then
+                        local id = card:get_id()
+                            if cardswithfaces[id] then
+                                cardswithfaces[id] = cardswithfaces[id] + 1
+                            else
+                                cardswithfaces[id] = 1
+                            end
                     end
-                    if card:get_id() == 12 then -- counting queens
-                        queenCount = queenCount + 1
-                    end
-                    if card:get_id() == 13 then -- counting queens
-                        kingCount = kingCount + 1
-                    end
+                -- print(cardswithfaces)
+                    -- if card:get_id() == 11 then --counting jacks
+                    --     jackCount = jackCount + 1
+                    -- end
+                    -- if card:get_id() == 12 then -- counting queens
+                    --     queenCount = queenCount + 1
+                    -- end
+                    -- if card:get_id() == 13 then -- counting queens
+                    --     kingCount = kingCount + 1
+                    -- end
                 end
 
-                if jackCount >= 3 or queenCount >= 3 or kingCount >= 3 then
-                return {
-                    xmult = card.ability.extra.Xmult
-                }
+                -- if jackCount >= 3 or queenCount >= 3 or kingCount >= 3 then
+                for _, numberofcards in pairs(cardswithfaces) do
+                    -- print(numberofcards)
+                    if numberofcards >= 3 then
+                    return {
+                        xmult = math.pi
+                    }
+                end
             end
         end 
     end
