04e2de632c91bb72b59aa32666da4adb0cf3113b
Jairus Azarael Bona <webmaster@milkyway.moe>
2025-11-02 19:35:58 +0800

    god there's a lot of betas for this release

---
+++ content/jokers/3_rare/cinnamoroll.lua
@@ -0,0 +1,55 @@
+SMODS.Joker{ --Cinnamoroll
+    key = "cinnamoroll",
+    config = {
+        extra = {
+            increment = 0.2,
+            chipmoroll = 1
+        }
+    },
+
+    loc_txt = {
+        ['name'] = 'Cinnamoroll',
+        ['text'] = {
+            'Gains {X:blue,C:white}#1#X{} {C:blue}Chips{} when a',
+            '{C:attention}Food Joker{} is {C:attention}eaten{}.',
+            '{C:inactive}(Currently {}{X:blue,C:white}#2#X{}{C:inactive} Chips){}',
+            '{C:inactive, s:1.2}Currently unimplemented, please check later.{}'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+
+    pos = {
+        x = 4,
+        y = 7
+    },
+
+    cost = 6,
+    rarity = 3,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'jokers',
+    pools = { 
+        ["milkys_jokers"] = true 
+    },
+
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.increment, card.ability.extra.chipmoroll } }
+    end,
+
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_type_destroyed then
+            
+        end
+
+        if context.cardarea == G.jokers and context.joker_main then
+            return {
+                    x_chips = card.ability.extra.chipmoroll
+                }
+        end
+    end
+}
